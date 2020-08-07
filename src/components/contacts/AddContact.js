af5ccddf2db951dc9756a99999adb778e564efb7
Raayhan <rayhan.rakib@northsouth.edu>
2020-08-07 23:34:59 +0600

    Created on event change for form submission

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -6,13 +6,18 @@ class AddContact extends Component {
     email: "",
     phone: "",
   };
+  onSubmit = (e) => {
+    e.preventDefault();
+  };
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
   render() {
     const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
@@ -21,6 +26,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Enter Name"
                 value={name}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -31,6 +37,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Enter Email"
                 value={email}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -41,6 +48,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Enter Phone"
                 value={phone}
+                onChange={this.onChange}
               />
             </div>
             <input
