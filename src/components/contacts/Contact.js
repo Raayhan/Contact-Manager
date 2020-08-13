04ce90e19e02fcee4b448571a2af12c971f5c334
Raayhan <rayhan.rakib@northsouth.edu>
2020-08-14 00:51:48 +0600

    create http requests for add, delete

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -6,10 +6,10 @@ class Contact extends Component {
   state = {
     showContactInfo: false,
   };
-  onDeleteClick = (id, dispatch) => {
-    axios
-      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => dispatch({ type: "DELETE_CONTACT", payload: id }));
+  onDeleteClick = async (id, dispatch) => {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+    dispatch({ type: "DELETE_CONTACT", payload: id });
   };
   render() {
     const { id, name, email, phone } = this.props.contact;
